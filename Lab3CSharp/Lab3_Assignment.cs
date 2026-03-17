283328fe73fc0d7ce0a6a6fa27f67f14431160e5
Diana Lysa <lysa.diana@chnu.edu.ua>
2026-03-17 21:30:47 +0200

    виконана 3 лабораторна

---
+++ Lab3CSharp/Lab3_Assignment.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab3_Assignment
+{
+    //ЗАВДАННЯ 1
+    public class Point
+    {
+        protected int x, y, c;
+
+        public Point() { x = 0; y = 0; c = 0; }
+        public Point(int x, int y, int c)
+        {
+            this.x = x; this.y = y; this.c = c;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Color => c;
+
+        public virtual void Display() =>
+            Console.WriteLine($"Точка: ({x}, {y}), Колір ID: {c}, Відстань: {GetDistance():F2}");
+
+        public double GetDistance() => Math.Sqrt(x * x + y * y);
+
+        public void Move(int x1, int y1) { x += x1; y += y1; }
+    }
+
+    // ЗАВДАННЯ 2
+    public class TransportVehicle
+    {
+        public string Brand { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public TransportVehicle(string brand, int maxSpeed)
+        {
+            Brand = brand; MaxSpeed = maxSpeed;
+        }
+
+        public virtual void Show() =>
+            Console.Write($"[{this.GetType().Name}] Бренд: {Brand}, Швидкість: {MaxSpeed} км/год");
+    }
+
+    public class Car : TransportVehicle
+    {
+        public string FuelType { get; set; }
+        public Car(string brand, int maxSpeed, string fuelType) : base(brand, maxSpeed) => FuelType = fuelType;
+        public override void Show() { base.Show(); Console.WriteLine($", Паливо: {FuelType}"); }
+    }
+
+    public class Train : TransportVehicle
+    {
+        public int Wagons { get; set; }
+        public Train(string brand, int maxSpeed, int wagons) : base(brand, maxSpeed) => Wagons = wagons;
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($", Вагонів: {Wagons}");
+        }
+
+        public class Express : Train
+        {
+            public string Route { get; set; }
+            public Express(string brand, int maxSpeed, int wagons, string route) : base(brand, maxSpeed, wagons) => Route = route;
+            public override void Show()
+            {
+                base.Show();
+                Console.WriteLine($", Маршрут: {Route}");
+            }
+        }
+    }
+}
--- Lab3CSharp/Program.cs
+++ Lab3CSharp/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace Lab3CSharp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lab3_Assignment;
+using static Lab3_Assignment.Train;
+
+namespace Lab3CSharp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab 3 ");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
+            {
+                Console.WriteLine("\n--- Лабораторна робота №3 ---");
+                Console.WriteLine("1. Завдання 1 (Точки)");
+                Console.WriteLine("2. Завдання 2 (Транспорт)");
+                Console.WriteLine("0. Вихід");
+                Console.Write("Ваш вибір: ");
+
+                string choice = Console.ReadLine();
+                if (choice == "1") RunTask1();
+                else if (choice == "2") RunTask2();
+                else if (choice == "0") break;
+            }
+        }
+
+        static void RunTask1()
+        {
+            Console.WriteLine("\n--- Результати Завдання 1 ---");
+            List<Point> points = new List<Point>
+            {
+                new Point(7, 4, 3),
+                new Point(0, 5, 2),
+                new Point(10, 10, 1),
+                new Point()
+            };
+
+            double avg = points.Average(p => p.GetDistance());
+            Console.WriteLine($"Середня відстань: {avg:F2}");
+
+            foreach (var p in points)
+            {
+                if (p.GetDistance() > avg) p.Move(5, 5);
+                p.Display();
+                p.Move(3, 4);
+            }
+        }
+
+        static void RunTask2()
+        {
+            Console.WriteLine("\n--- Результати Завдання 2 ---");
+            TransportVehicle[] fleet = {
+                new Car("Audi", 220, "Бензин"),
+                new Express("Intercity", 160, 9, "Київ-Львів"),
+                new Train("Вантажний", 80, 30)
+            };
+
+            foreach (var v in fleet.OrderBy(x => x.GetType().Name))
+            {
+                v.Show();
+            }
         }
     }
 }
